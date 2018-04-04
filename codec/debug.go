c764ed77405169b8fe3bc311c84d1465f4194b7d
Henry <cryptix@riseup.net>
2018-04-04 21:05:45 +0200

    gofmt and debug Wrap() log correction

--- codec/debug.go
+++ codec/debug.go
@@ -28,39 +28,39 @@ import (
 func Wrap(l log.Logger, rwc io.ReadWriteCloser) io.ReadWriteCloser {
 	prout, pwout := io.Pipe()
 	go func() {
-		lout := log.With(l, "unit", "pipeFrom")
+		from := log.With(l, "unit", "pipeFrom")
 		r := NewReader(io.TeeReader(rwc, pwout))
 		for {
 			pkt, err := r.ReadPacket()
 			if err != nil {
-				lout.Log("error", err)
+				from.Log("error", err)
 				//pwout.CloseWithError(err)
 				return
 			}
-			lout.Log("pkt", fmt.Sprintf("%+v", pkt))
+			from.Log("pkt", fmt.Sprintf("%+v", pkt))
 		}
 	}()
 
 	prin, pwin := io.Pipe()
 	w := NewWriter(rwc)
 	go func() {
-		lin := log.With(l, "unit", "pipeFrom")
+		to := log.With(l, "unit", "pipeTo")
 		r := NewReader(prin)
 		for {
 			pkt, err := r.ReadPacket()
 			if err != nil {
 				if err != io.EOF {
-					lin.Log("action", "ReadPacket", "error", err)
+					to.Log("action", "ReadPacket", "error", err)
 					prin.CloseWithError(err)
 				}
 				return
 			}
 			if err := w.WritePacket(pkt); err != nil {
-				lin.Log("action", "WritePacket", "error", err)
+				to.Log("action", "WritePacket", "error", err)
 				prin.CloseWithError(err)
 				return
 			}
-			lin.Log("pkt", fmt.Sprintf("%+v", pkt))
+			to.Log("pkt", fmt.Sprintf("%+v", pkt))
 		}
 	}()
 	return struct {
