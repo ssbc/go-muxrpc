2f9afed55c18c689f5eae79e81bbf31f5dcc84b5
Henry <cryptix@riseup.net>
2019-05-07 18:34:02 +0200

    only log 'serve returning' if there is an error

--- rpc.go
+++ rpc.go
@@ -363,10 +363,10 @@ func (r *rpc) Serve(ctx context.Context) (err error) {
 			r.rLock.Lock()
 			defer r.rLock.Unlock()
 			if n := len(r.reqs); n > 0 {
-				log.Printf("muxrpc(%v): serve loop returning (%v) - closing open reqs: %d", r.remote, err, n)
 				if err == nil {
 					cerr = errors.Errorf("muxrpc: unexpected end of session")
 				} else {
+					log.Printf("muxrpc(%v): serve loop returning (%v) - closing open reqs: %d", r.remote, err, n)
 					cerr = err
 				}
 				for id, req := range r.reqs {
