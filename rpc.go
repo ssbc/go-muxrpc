b4207658b847a2bf26c9a0c119e0d2abedca07b7
Henry <cryptix@riseup.net>
2019-09-27 14:53:42 +0200

    increase bufSize

--- rpc.go
+++ rpc.go
@@ -43,7 +43,10 @@ type rpc struct {
 	tLock      sync.Mutex
 }
 
-const bufSize = 100
+// this sets the buffer size of individual request streams
+// if the other side of stream doesn't read it's messages
+// the read loop stops draining and all other replies might deadlock 
+const bufSize = 5000
 
 // Handle handles the connection of the packer using the specified handler.
 func Handle(pkr Packer, handler Handler) Endpoint {
