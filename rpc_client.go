77ceac08eceae06ae47d6e69e7e84f326ed7d9c2
Henry <cryptix@riseup.net>
2020-12-22 12:26:16 +0100

    move Handle clients to rpc_server

--- rpc_client.go
+++ rpc_client.go
@@ -8,120 +8,13 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"net"
-	"os"
 
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
-	"github.com/karrick/bufpool"
 	"github.com/pkg/errors"
 
 	"go.cryptoscope.co/muxrpc/v2/codec"
 )
-
-type HandleOption func(*rpc)
-
-func WithContext(ctx context.Context) HandleOption {
-	return func(r *rpc) {
-		r.serveCtx = ctx
-	}
-}
-
-// WithRemoteAddr also sets the remote address the endpoint is connected to.
-// ie if the packer tunnels through something which can't see the address.
-func WithRemoteAddr(addr net.Addr) HandleOption {
-	return func(r *rpc) {
-		r.remote = addr
-	}
-}
-
-// WithLogger let's you overwrite the stderr logger
-func WithLogger(l log.Logger) HandleOption {
-	return func(r *rpc) {
-		r.logger = l
-	}
-}
-
-func WithIsServer(yes bool) HandleOption {
-	return func(r *rpc) {
-		r.isServer = yes
-	}
-}
-
-// IsServer tells you if the passed endpoint is in the server-role or not.
-// i.e.: Did I call the remote: yes.
-// Was I called by the remote: no.
-// Q: don't want to extend Endpoint interface?
-func IsServer(edp Endpoint) bool {
-	rpc, ok := edp.(*rpc)
-	if !ok {
-		panic(fmt.Sprintf("not an *rpc: %T", edp))
-	}
-
-	return rpc.isServer
-}
-
-// Handle handles the connection of the packer using the specified handler.
-func Handle(pkr *Packer, handler Handler, opts ...HandleOption) Endpoint {
-	r := &rpc{
-		pkr:  pkr,
-		reqs: make(map[int32]*Request),
-		root: handler,
-	}
-
-	for _, o := range opts {
-		o(r)
-	}
-	// defaults
-	if r.logger == nil {
-		logger := log.NewLogfmtLogger(os.Stderr)
-		logger = level.NewFilter(logger, level.AllowInfo()) // only log info and above
-		r.logger = log.With(logger, "ts", log.DefaultTimestampUTC, "unit", "muxrpc")
-	}
-
-	if r.remote == nil {
-		if ra, ok := pkr.c.(interface{ RemoteAddr() net.Addr }); ok {
-			r.remote = ra.RemoteAddr()
-		}
-	}
-
-	if r.remote != nil {
-		// TODO: retract remote address
-		r.logger = log.With(r.logger, "remote", r.remote.String())
-	}
-
-	if r.serveCtx == nil {
-		r.serveCtx = context.Background()
-	}
-
-	bp, err := bufpool.NewChanPool()
-	if err != nil {
-		panic(err)
-	}
-	r.bpool = bp
-
-	// we need to be able to cancel in any case
-	r.serveCtx, r.cancel = context.WithCancel(r.serveCtx)
-
-	go handler.HandleConnect(r.serveCtx, r)
-
-	return r
-}
-
-// no args should be handled as empty array not args: null
-func marshalCallArgs(args []interface{}) ([]byte, error) {
-	var argData []byte
-	if len(args) == 0 {
-		argData = []byte("[]")
-	} else {
-		var err error
-		argData, err = json.Marshal(args)
-		if err != nil {
-			return nil, errors.Wrap(err, "error marshaling request arguments")
-		}
-	}
-	return argData, nil
-}
 
 // Async does an aync call on the remote.
 func (r *rpc) Async(ctx context.Context, ret interface{}, re RequestEncoding, method Method, args ...interface{}) error {
