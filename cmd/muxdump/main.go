61fd0f5e7ba4dc4fb2dc716a3091b2e4e869af70
Henry <cryptix@riseup.net>
2021-09-21 07:37:52 +0200

    muxdump: add -req and -maxlen flags
    * req filters out requests
    * maxlen controls the maximum length of the bodies

--- cmd/muxdump/main.go
+++ cmd/muxdump/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"encoding/hex"
+	"flag"
 	"fmt"
 	"io"
 	"os"
@@ -20,6 +22,12 @@ func check(err error) {
 }
 
 func main() {
+	var req int
+	var maxLen uint
+	flag.IntVar(&req, "req", 0, "which request to filter out (default means all requests)")
+	flag.UintVar(&maxLen, "maxlen", 256, "maximum length of a body, rest is truncated")
+	flag.Parse()
+
 	rd := codec.NewReader(os.Stdin)
 
 	var (
@@ -37,8 +45,7 @@ func main() {
 			return
 		}
 
-		fmt.Printf("\n  [Req: %04d Size:%s %s\n", hdr.Req, humanize.Bytes(uint64(hdr.Len)), hdr.Flag)
-
+		bodyBuf.Reset()
 		err = rd.ReadBodyInto(bodyBuf, hdr.Len)
 		if err != nil {
 			if errors.Cause(err) == io.EOF {
@@ -47,22 +54,27 @@ func main() {
 			check(fmt.Errorf("failed to read body: %w", err))
 			return
 		}
-		body := bodyBuf.Bytes()
 
-		if hdr.Flag.Get(codec.FlagString) || hdr.Flag.Get(codec.FlagJSON) {
-			if len(body) > 250 {
-				body = body[:250]
-			}
-			fmt.Println(string(body))
-		} else {
-			if len(body) > 32 {
-				body = body[:32]
-			}
-			fmt.Printf("%x\n", body)
+		if req != 0 && int(hdr.Req) != req {
+			continue
 		}
 
-		bodyBuf.Reset()
+		fmt.Printf("\n  [Req: %04d Size:%s %s\n", hdr.Req, humanize.Bytes(uint64(hdr.Len)), hdr.Flag)
 
+		body := bodyBuf.Bytes()
+		if uint(len(body)) > maxLen {
+			body = append(body[:maxLen], '.', '.', '.')
+		}
+
+		var out string
+		switch {
+		case hdr.Flag.Get(codec.FlagJSON):
+			out = string(body)
+		default:
+			out = hex.Dump(body)
+		}
+		fmt.Println(out)
 	}
+
 	fmt.Println("done")
 }
